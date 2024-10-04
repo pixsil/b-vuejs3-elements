9764db4066f4b3a520f695d2027b57a8d3b76984
Luke Braakman <luke@pixsil.nl>
2024-10-04 12:37:27 +0200

    Made NPM package of it

--- src/index.js
+++ src/index.js
@@ -1,30 +1,59 @@
-import BBreadcrumb from './components/b-breadcrumb.vue';
-import BButton from './components/b-button.vue';
-import BCard from './components/b-card.vue';
-import BCardBody from './components/b-card-body.vue';
-import BFormCheckbox from './components/b-form-checkbox.vue';
-import BFormFile from './components/b-form-file.vue';
-import BFormGroup from './components/b-form-group.vue';
-import BFormInput from './components/b-form-input.vue';
-import BFormSelect from './components/b-form-select.vue';
-import BFormSelectOption from './components/b-form-select-option.vue';
-import BFormTextarea from './components/b-form-textarea.vue';
-import BInputGroup from './components/b-input-group.vue';
-import BListGroup from './components/b-list-group.vue';
-import BListGroupItem from './components/b-list-group-item.vue';
-import BModal from './components/b-modal.vue';
-import BOverlay from './components/b-overlay.vue';
-import BProgress from './components/b-progress.vue';
-import BTable from './components/b-table.vue';
-import BTableSimple from './components/b-table-simple.vue';
-import BTbody from './components/b-tbody.vue';
-import BTd from './components/b-td.vue';
-import BTh from './components/b-th.vue';
-import BThead from './components/b-thead.vue';
-import BTr from './components/b-tr.vue';
-import BWrapper from './components/b-wrapper.vue';
+// Boostrap Components
+import BBreadcrumb from './components/boostrap/b-breadcrumb.vue';
+import BButton from './components/boostrap/b-button.vue';
+import BCard from './components/boostrap/b-card.vue';
+import BCardBody from './components/boostrap/b-card-body.vue';
+import BFormCheckbox from './components/boostrap/b-form-checkbox.vue';
+import BFormFile from './components/boostrap/b-form-file.vue';
+import BFormGroup from './components/boostrap/b-form-group.vue';
+import BFormInput from './components/boostrap/b-form-input.vue';
+import BFormSelect from './components/boostrap/b-form-select.vue';
+import BFormSelectOption from './components/boostrap/b-form-select-option.vue';
+import BFormTextarea from './components/boostrap/b-form-textarea.vue';
+import BInputGroup from './components/boostrap/b-input-group.vue';
+import BListGroup from './components/boostrap/b-list-group.vue';
+import BListGroupItem from './components/boostrap/b-list-group-item.vue';
+import BModal from './components/boostrap/b-modal.vue';
+import BOverlay from './components/boostrap/b-overlay.vue';
+import BProgress from './components/boostrap/b-progress.vue';
+import BTable from './components/boostrap/b-table.vue';
+import BTableSimple from './components/boostrap/b-table-simple.vue';
+import BTbody from './components/boostrap/b-tbody.vue';
+import BTd from './components/boostrap/b-td.vue';
+import BTh from './components/boostrap/b-th.vue';
+import BThead from './components/boostrap/b-thead.vue';
+import BTr from './components/boostrap/b-tr.vue';
+import BWrapper from './components/boostrap/b-wrapper.vue';
+
+// Tailwind Components
+import TBreadcrumb from './components/tailwind/b-breadcrumb.vue';
+import TButton from './components/tailwind/b-button.vue';
+import TCard from './components/tailwind/b-card.vue';
+import TCardBody from './components/tailwind/b-card-body.vue';
+import TFormCheckbox from './components/tailwind/b-form-checkbox.vue';
+import TFormFile from './components/tailwind/b-form-file.vue';
+import TFormGroup from './components/tailwind/b-form-group.vue';
+import TFormInput from './components/tailwind/b-form-input.vue';
+import TFormSelect from './components/tailwind/b-form-select.vue';
+import TFormSelectOption from './components/tailwind/b-form-select-option.vue';
+import TFormTextarea from './components/tailwind/b-form-textarea.vue';
+import TInputGroup from './components/tailwind/b-input-group.vue';
+import TListGroup from './components/tailwind/b-list-group.vue';
+import TListGroupItem from './components/tailwind/b-list-group-item.vue';
+import TModal from './components/tailwind/b-modal.vue';
+import TOverlay from './components/tailwind/b-overlay.vue';
+import TProgress from './components/tailwind/b-progress.vue';
+import TTable from './components/tailwind/b-table.vue';
+import TTableSimple from './components/tailwind/b-table-simple.vue';
+import TTbody from './components/tailwind/b-tbody.vue';
+import TTd from './components/tailwind/b-td.vue';
+import TTh from './components/tailwind/b-th.vue';
+import TThead from './components/tailwind/b-thead.vue';
+import TTr from './components/tailwind/b-tr.vue';
+import TWrapper from './components/tailwind/b-wrapper.vue';
 
 export {
+    // Boostrap components
     BBreadcrumb,
     BButton,
     BCard,
@@ -49,5 +78,32 @@ export {
     BTh,
     BThead,
     BTr,
-    BWrapper
+    BWrapper,
+
+    // Tailwind components
+    TBreadcrumb,
+    TButton,
+    TCard,
+    TCardBody,
+    TFormCheckbox,
+    TFormFile,
+    TFormGroup,
+    TFormInput,
+    TFormSelect,
+    TFormSelectOption,
+    TFormTextarea,
+    TInputGroup,
+    TListGroup,
+    TListGroupItem,
+    TModal,
+    TOverlay,
+    TProgress,
+    TTable,
+    TTableSimple,
+    TTbody,
+    TTd,
+    TTh,
+    TThead,
+    TTr,
+    TWrapper
 };
